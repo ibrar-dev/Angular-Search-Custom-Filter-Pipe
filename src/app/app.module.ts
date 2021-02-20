a4ef3389b63f6fb66444934feb626096bfe182f5
muhammadibrar1 <muhammadibrar1@yahoo.com>
2021-02-20 09:33:50 +0500

    1 file modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { PipeModule } from './_pipe/pipe.module';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { PipeModule } from "./pipes/pipe.module";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, PipeModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, PipeModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
